dd8ceb3024998332daa063e84ad8b67acb4be71c
vanshika <vanshikagaikwad28@gmail.com>
2026-06-19 00:58:35 +0530

    Connected Render backend

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fullstack-task-manager-obpn.onrender.com/api"
 });
 
 export default API;
